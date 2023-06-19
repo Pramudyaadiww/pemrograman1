978af85f60ed824180eb9537ebbd303ea1eb05a6
Pramudya Adi Wicaksono <133669606+Pramudyaadiww@users.noreply.github.com>
2023-06-19 11:38:03 +0700

    Delete modul3 directory

--- modul3/validasi_nilai.py
+++
@@ -1,9 +0,0 @@
-bilangan = int(input("Masukan bilangan: "))
-pembagi = int(input("Masukan bilangan pembagi: "))
-
-if(pembagi == 0):
-    print("Pembagi tidak boleh 0")
-else:
-    print(bilangan / pembagi)
-
-    
--- modul3/vokal_dan_konsonan.py
+++
@@ -1,7 +0,0 @@
-huruf = input("Masukkan huruf: ")
-if huruf in ['a', 'i', 'u', 'e', 'o']:
-    print(huruf, "adalah huruf vokal")
-else:
-    print(huruf, "adalah huruf konsonan")
-
-
